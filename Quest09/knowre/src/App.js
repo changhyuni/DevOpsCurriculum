06133c1727ab4650431cb516118ba5ea485b9ee8
changhyuni <bhs9610@naver.com>
2021-03-11 22:21:38 +0900

    3.11

--- Quest09/knowre/src/App.js
+++ Quest09/knowre/src/App.js
@@ -1,36 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  componentDidMount() {
-        fetch('http://knowre-cloudfront-elb-60923550.ap-northeast-2.elb.amazonaws.com/api')
-            .then(res=>res.json())
-            .then(data=>this.setState({username:data.username}));
-    }
-    
-    render(){
-    	// ...
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        username:null,
+        num: 0
+    };
+}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            username:null
-        };
-    }
+
+shoot() {
+  let result = document.getElementById('result');
+  result.innerText = "Good job";
+}
+    componentDidMount() {
+      fetch('http://knowre-cloudfront-elb-60923550.ap-northeast-2.elb.amazonaws.com/api')
+      .then(res=>res.json())
+      .then(data=>this.setState({username:data.username}));
+  }
 
   render() {
     const {username} = this.state;
-    return (
-        <div className="App">
-          <header className="App-header">
-            {username ? `Hello ${username}` : 'Hello World'}
-          </header>
-        </div>
+    const myelement = (
+      <button onClick={this.shoot}>testing!</button>
     );
-    ;
-  }
-}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{username ? `Hello ${username}` : 'Hello World'}</p>
+          <button id='button' className='button' onClick={()=> 
+          {this.setState((state)=>{
+            return {num: state.num+1}
+          })}
+        }
+          >Do you want testing?</button>
+          <div id="result"></div>
+        </header>
+      </div>
+    ) ;
+    };
+ 
+ }
+ 
+
+  
+
 
 export default App;
--- Quest09/server/server.js
+++ Quest09/server/server.js
@@ -3,12 +3,17 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const port =process.env.PORT || 3001;
+const user = {username:'changhyun'}
 
 app.use(cors());
-
-app.use(bodyParser.json());
-app.use('/api', (req, res)=> res.json({username:'changhyun'}));
+app.get('/api', (req, res) => {
+    res.send(user);
+  });
+// app.use(bodyParser.json());
+// app.use('/api', (req, res)=> res.json(user));
 
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
 })
+
+module.exports = app;
